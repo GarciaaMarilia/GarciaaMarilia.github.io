06bd70bdf6bc327ed6ebdc9e8f20aa709594bdf4
marilia.garcia <marilia.garcia@logiquesistemas.com.br>
2024-07-04 15:51:33 +0200

    changing names

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@ function changeLanguage(language) {
     document.documentElement.lang = language;
     if (language === 'en') {
         document.querySelector('h2').textContent = 'Hello, visitor! 👋';
-        document.querySelector('p').textContent = 'My name is Yuri, Front-end Developer';
+        document.querySelector('p').textContent = 'My name is Marília Garcia, Front-end Developer';
         document.querySelectorAll('nav a')[0].textContent = 'Home';
         document.querySelectorAll('nav a')[1].textContent = 'About';
         document.querySelectorAll('nav a')[2].textContent = 'Projects';
         document.querySelectorAll('nav a')[3].textContent = 'Contact';
     } else if (language === 'fr') {
         document.querySelector('h2').textContent = 'Bonjour, visiteur! 👋';
-        document.querySelector('p').textContent = 'Je m\'appelle Yuri, Développeur Front-end';
+        document.querySelector('p').textContent = 'Je m\'appelle Marília Garcia, Développeur Front-end';
         document.querySelectorAll('nav a')[0].textContent = 'Accueil';
         document.querySelectorAll('nav a')[1].textContent = 'À propos';
         document.querySelectorAll('nav a')[2].textContent = 'Projets';
         document.querySelectorAll('nav a')[3].textContent = 'Contact';
     } else {
         document.querySelector('h2').textContent = 'Olá, visitante! 👋';
-        document.querySelector('p').textContent = 'Meu nome é Yuri, Desenvolvedor Front-end';
+        document.querySelector('p').textContent = 'Meu nome é Marília Garcia, Desenvolvedor Front-end';
         document.querySelectorAll('nav a')[0].textContent = 'Início';
         document.querySelectorAll('nav a')[1].textContent = 'Sobre';
         document.querySelectorAll('nav a')[2].textContent = 'Projetos';
